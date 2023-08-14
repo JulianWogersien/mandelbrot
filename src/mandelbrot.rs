0da422991d310688f72e4bbbb133a9d9a83da3a6
coolian1337 <julian.wogersien@gmail.com>
2023-08-14 16:09:23 +0200

    cleanup

--- src/mandelbrot.rs
+++ src/mandelbrot.rs
@@ -1,10 +1,7 @@
-use std::fmt::format;
-use std::time::{Instant, SystemTime};
 use std::{convert::TryInto, thread::JoinHandle};
 use std::thread;
 
 use num::{Complex, complex::ComplexFloat};
-use sfml::system::Time;
 use sfml::{graphics::{Drawable, Sprite, Image, Texture, Rect, Color}, SfBox, system::Vector2i};
 
 use crate::math::math;
